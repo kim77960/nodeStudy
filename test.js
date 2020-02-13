c3c51a8acb9ddf79546ae12f79bd116dcf8a1aa3
unknown <kim77960>
2020-02-13 16:32:20 +0900

    2/13

--- test.js
+++ test.js
@@ -1,10 +1,38 @@
-'use strict';
-const animal = {
-  dog: 1,
-  cat: 2,
-  rabbit: 3,
-};
-const animalChecker = () => (1 ? true : false);
-console.log(animalChecker());
-const { dog, cat, rabbit } = animal;
-console.log(dog);
+const todos = [
+  {
+    id: 1,
+    text: '자바스크립트 입문',
+    done: true,
+  },
+  {
+    id: 2,
+    text: '함수 배우기',
+    done: true,
+  },
+  {
+    id: 3,
+    text: '객체와 배열 배우기',
+    done: true,
+  },
+  {
+    id: 4,
+    text: '배열 내장함수 배우기',
+    done: false,
+  },
+];
+
+todos.map(({ id }) => {
+  console.log(id);
+});
+
+const [a, b, c, d] = todos;
+
+const { id } = a;
+
+console.log(id);
+
+// ()=>{}
+
+todos.map(function({ text }) {
+  console.log(text);
+});
